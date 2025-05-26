6df575e224aabd047df82c60234d966be18d2cff
Drapesnape <elilow13610@gmail.com>
2025-05-26 14:55:37 -0400

    aaaaa

---
+++ constants/types/events.ts
@@ -0,0 +1,28 @@
+type Items = {
+    id: string;
+    cafe_id: string;
+    title: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+    active_until: string;
+    tags: string[];
+    author: Author;
+    interactions: any[];
+}
+
+type Author = {
+    id: string;
+    username: string;
+    email: string;
+    matricule: string;
+    first_name: string;
+    last_name: string;
+    photo_url: string;
+}
+
+type EventsData = {
+    items: Items[];
+}
+
+export type { Items, Author, EventsData };
